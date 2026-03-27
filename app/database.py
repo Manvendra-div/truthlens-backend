06a47e638d8c5bbfa5d9cbe30687959c4e70ef77
Manvendra singh <ms1577239@gmail.com>
2026-03-27 21:49:09 +0530

    Remove environment check for printing DATABASE_URL in database.py

--- app/database.py
+++ app/database.py
@@ -8,8 +8,7 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 ENV = os.getenv("ENV")
 
-if ENV=="DEV":
-    print(DATABASE_URL)
+print(DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 
